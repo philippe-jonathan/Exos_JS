1611b74f8a9249d0751ba75ae8610afdceef1608
philippe <philippe@SIMOCCAUCH26.lan>
2018-06-11 14:24:06 +0200

    Exos-groupe

--- exos.js
+++ exos.js
@@ -1,7 +1,34 @@
-var maVar=3;
-alert(maVar);
+//Exo 1
+/*
 
+var maVar=3;
+	alert(maVar);
+
+//Exo 2
 var nom="Philippe";
 var prenom="Jonathan";
 var ville="Aubiet";
-alert("Nom: " + nom + "\nPrenom: " + prenom + "\nVille: " + ville);
+	alert("Nom: " + nom + "\nPrenom: " + prenom + "\nVille: " + ville);
+
+//Exo 3
+var nom1 = prompt("quel est ton prenom?");
+	alert("Bonjour, " + nom1);
+
+//Exo 4
+var Nom = prompt("Quel est ton nom?");
+var Prenom = prompt("Quel est ton prenom?");
+var Ville = prompt("Où habite tu?");
+	alert("Bonjour, " + Nom + " " + Prenom + " tu habite à " + Ville);
+
+
+//Exo 5
+var nbr = prompt("Donner un nombre a virgule.");
+var nbr2 = prompt("Donner un dexieme nombre a virgule.");
+nbr = parseInt(nbr);
+nbr2 = parseInt(nbr2);
+	alert("Voici vos nombres: " + nbr + " et " + nbr2);
+var result = (nbr * nbr2);
+	alert(result);
+*/
+
+//Exo 6
