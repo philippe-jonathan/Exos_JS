374f1da5523e87ef88ea419c12948cfa511f5386
philippe <philippe@SIMOCCAUCH26.lan>
2018-06-11 15:17:15 +0200

    Exos-groupe

--- exos2.js
+++ exos2.js
@@ -1,8 +1,25 @@
-//Exo 6
+//Exo 6 
+/*
 var NBR = prompt("Donner un nombre a virgule.");
-var NBR2 = prompt("Donner un dexieme nombre a virgule.");
+var NBR2 = prompt("Donner un deuxieme nombre a virgule.");
 NBR = parseInt(NBR);
 NBR2 = parseInt(NBR2);
 	alert("Voici vos nombres: " + NBR + " et " + NBR2);
 var result = (NBR % NBR2);
 	alert(result);
+*/
+
+//Exo 7
+/*
+var result = prompt("donne t'a pointure ") * 2 + 5 * 50 - prompt("donne t'on année de naissance") + 1766;
+	alert(result);
+*/
+
+//Exo 8
+var age = prompt("donner votre age");
+
+	if (age>=18) { 
+		alert("Vous etes majeur!");
+	}else{
+		alert("Vous etes mineur!");
+	}
