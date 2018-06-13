4cb961484b68c866601e100f6c948953122e1d9a
philippe <philippe@SIMOCCAUCH26.lan>
2018-06-13 16:00:43 +0200

    avancement2

--- JS-exercice-master/loop/LOOP2.js
+++ JS-exercice-master/loop/LOOP2.js
@@ -1,46 +1,51 @@
-function var1(){
-	var nbr = 0;
-		while(nbr < 10){
-			alert(nbr);
-			nbr++;
-		}
+window.onload = function onclick(){
+	document.getElementById("demo1").addEventListener("click", function var1(){
+		var nbr = 0;
+			while(nbr < 10){
+				alert(nbr);
+				nbr++;
+			}
+}	);
 }
 
-
-
-function var2(){
-	var NBR = 0;
-	var NBR2 = 2;
-		for(NBR = 0; NBR<20; NBR++){
-			var NBR = NBR * NBR2;
-			alert(NBR);
-		}
+window.onload = function onclick2(){
+	document.getElementById("demo2").addEventListener("click", function var2(){
+		var NBR = 0;
+		var NBR2 = 2;
+			for(NBR = 0; NBR<20; NBR++){
+				var NBR = NBR * NBR2;
+					alert(NBR);
+			}
+	}	
+	)
 }
 
-
-
-function var3(){
+window.onload = function onclick3(){
+document.getElementById("demo3").addEventListener("click", function var3(){
 	var num = 100;
 	var num2 = 23;
 		for(num = 100; num>=10; num--){
 			var result = num * num2;
 			alert(result);
 		}
+})
 }
 
 
 
-function var4(){
+window.onload = function onclick4(){
+	document.getElementById("demo4").addEventListener("click", function var4(){
 	var NUM = 1;
 	while(NUM < 10){
 		alert(NUM);
 		NUM += NUM / 2;
 	}
+})
 }
 
 
-
-function var5(){
+window.onload = function onclick5(){
+	document.getElementById("demo5").addEventListener("click", function var5(){
 	var OK = 1;
 		while(OK < 15){
 			alert(OK);
@@ -50,11 +55,11 @@ function var5(){
 				alert("on y arrive presque");
 			} 
 		}
+})
 }
 
-
-
-function var6(){
+window.onload = function onclick6(){
+	document.getElementById("demo6").addEventListener("click", function var6(){
 	var OK = 20;
 		while(OK > 0){
 			alert(OK + " c'est presque bon");
@@ -63,11 +68,11 @@ function var6(){
  				alert("!! BOOM !!");
  			}
 		}
+})
 }
 
-
-
-function var7(){
+window.onload = function onclick7(){
+	document.getElementById("demo7").addEventListener("click", function var7(){
 	var jojo = 1;
 	while(jojo < 100){
 		alert(jojo);
@@ -77,11 +82,11 @@ function var7(){
 			alert("On tien le bon bout")
 		}
 	}
+})
 }
 
-
-
-function var8(){
+window.onload = function onclick8(){
+	document.getElementById("demo8").addEventListener("click", function var8(){
 	var JOJO = 200;
 		while(JOJO > 0){
 			alert(JOJO + " Enfin !!!!");
@@ -90,4 +95,5 @@ function var8(){
  				alert("!! BOOM !!");
  			}
 		}
+})
 }
